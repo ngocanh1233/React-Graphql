c16c47a1c75b9bc8bb7b0b1ceee75352bc4156ac
ngocanh1233 <ngocanh1233@gmail.com>
2017-09-18 16:15:52 +0700

    2
    2

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  gql,
+  graphql,
+  ApolloProvider,
+} from 'react-apollo';
+const client = new ApolloClient();
+
+
+const ChannelsList = ({ data: {loading, error, channels }}) => {
+   if (loading) {
+     return <p>Loading ...</p>;
+   }
+   if (error) {
+     return <p>{error.message}</p>;
+   }
+   return <ul>
+     { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
+   </ul>;
+ };
+const channelsListQuery = gql`
+        query ChannelsListQuery {
+          channels {
+            id
+            name
+          }
+        }
+      `;
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
+   render() {
+     return (
+       <ApolloProvider client={client}>
+         <div className="App">
+           <div className="App-header">
+             <img src={logo} className="App-logo" alt="logo" />
+             <h2>Welcome to Apollo</h2>
+           </div>
+           <ChannelsListWithData />
+         </div>
+       </ApolloProvider>
+     );
+   }
+ }
 export default App;
